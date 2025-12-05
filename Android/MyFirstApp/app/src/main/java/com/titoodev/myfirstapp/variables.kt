d2175dd5c982cd5736b2e325cffa0643e95b4006
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-12-05 11:29:59 -0500

    my first project in android

---
+++ Android/MyFirstApp/app/src/main/java/com/titoodev/myfirstapp/variables.kt
@@ -0,0 +1,33 @@
+package com.titoodev.myfirstapp
+// este es el primer archivo que creamos.
+// lo creamos de tipo "file", porque es de los archivos mas "simples" para trabajar
+
+// emepezaremos hablando sobre los tipos de variables en kotlin
+// es muy similar a python, el por si mismo es capaz de entender que tipo de variable estaremos usando, pero de todas formas podriamos definirlas!
+
+
+fun main(){
+    /**
+     * VARIABLES NUMERICAS
+     */
+
+    // Valor entero ( numeros enteros relativamente no grandes )
+        val entero:Int = 10
+
+    // Valor Long ( numero enteros "grandes" )
+        val grande:Long = 30
+
+    // Flotante ( numeros con pocos decimales )
+        val flotante:Float = 20.4f
+
+    // Double ( numero con muchosss decimales )
+        val decimales:Double = 20.42332
+
+    /**
+     * VARIABLES ALFANUMERICAS
+     */
+
+    // Char, solo soporta un caracter
+    val caracter:Char = 'A'
+    
+}
